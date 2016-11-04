1df7ecdc05c4db76edd079486d51d1776b8ab429
joaoferreira <joaoferreira>
2016-11-04 09:55:43 -0200

    Alteração dos sql

--- project-files/create_tables.sql
+++ project-files/create_tables.sql
@@ -5,19 +5,19 @@ USE school_life ;
 
 CREATE TABLE `CD_ROLE` (
   `id_role` int(11) NOT NULL AUTO_INCREMENT,
-  `name_role` varchar(45) DEFAULT NULL,
+  `role_name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id_role`)
 
 ) ;
 
 CREATE TABLE `CD_USER` (
   `registration_user` int(11) NOT NULL,
-  `login` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `name` varchar(90) DEFAULT NULL,
-  `birthDate` date DEFAULT NULL,
+  `email` varchar(128) NOT NULL UNIQUE,
+  `password` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `birthDate` date NOT NULL,
   `docs` blob,
-  `id_role` int(11) DEFAULT NULL,
+  `id_role` int(11) NOT NULL,
   PRIMARY KEY (`registration_user`),
   KEY `id_role` (`id_role`),
   CONSTRAINT `CD_USER_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `CD_ROLE` (`id_role`)
@@ -26,7 +26,7 @@ CREATE TABLE `CD_USER` (
 
 CREATE TABLE `CD_DISC` (
   `id_disc` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(90) DEFAULT NULL,
+  `name` varchar(90) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `registration_user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_disc`),
@@ -34,30 +34,45 @@ CREATE TABLE `CD_DISC` (
   CONSTRAINT `CD_DISC_ibfk_1` FOREIGN KEY (`registration_user`) REFERENCES `CD_USER` (`registration_user`)
 ) ;
 
-CREATE TABLE `RL_STUDENT_DISC` (
-  `registration_user` int(11) DEFAULT NULL,
-  `id_role` int(11) DEFAULT NULL,
+
+CREATE TABLE `CD_ABSENCE` (
+  `id_absence` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `id_disc` int(11) not null,
+  `registration_user` int(11) not null,
+  PRIMARY KEY (`id_absence`),
+  KEY `id_disc` (`id_disc`),
   KEY `registration_user` (`registration_user`),
-  KEY `id_role` (`id_role`),
+  CONSTRAINT `CD_ABSENCE_ibfk_1` FOREIGN KEY (`id_disc`) REFERENCES `CD_DISC` (`id_disc`),
+  CONSTRAINT `CD_ABSENCE_ibfk_2` FOREIGN KEY (`registration_user`) REFERENCES `CD_USER` (`registration_user`)
+) ;
+
+
+CREATE TABLE `RL_STUDENT_DISC` (
+  `registration_user` int(11) NOT NULL,
+  `id_disc` int(11) NOT NULL,
+  KEY `registration_user` (`registration_user`),
+  KEY `id_disc` (`id_disc`),
   CONSTRAINT `RL_STUDENT_DISC_ibfk_1` FOREIGN KEY (`registration_user`) REFERENCES `CD_USER` (`registration_user`),
-  CONSTRAINT `RL_STUDENT_DISC_ibfk_2` FOREIGN KEY (`id_role`) REFERENCES `CD_ROLE` (`id_role`)
+  CONSTRAINT `RL_STUDENT_DISC_ibfk_2` FOREIGN KEY (`id_disc`) REFERENCES `CD_DISC` (`id_disc`)
 ) ;
 
 CREATE TABLE `RL_STUDENT_RESP` (
-  `registration_user` int(11) DEFAULT NULL,
-  `registration_user_resp` int(11) DEFAULT NULL,
+  `registration_user` int(11) NOT NULL,
+  `registration_user_resp` int(11) NOT NULL,
   KEY `registration_user` (`registration_user`),
   KEY `registration_user_resp` (`registration_user_resp`),
+  PRIMARY KEY (`registration_user`, `registration_user_resp`),
   CONSTRAINT `RL_STUDENT_RESP_ibfk_1` FOREIGN KEY (`registration_user`) REFERENCES `CD_USER` (`registration_user`),
   CONSTRAINT `RL_STUDENT_RESP_ibfk_2` FOREIGN KEY (`registration_user_resp`) REFERENCES `CD_USER` (`registration_user`)
 ) ;
 
 CREATE TABLE `CD_GRADE` (
   `id_grade` int(11) NOT NULL,
-  `id_disc` int(11) DEFAULT NULL,
-  `registration_user` int(11) DEFAULT NULL,
-  `nota` double DEFAULT NULL,
-  `semestre` varchar(10) DEFAULT NULL,
+  `id_disc` int(11) NOT NULL,
+  `registration_user` int(11) NOT NULL,
+  `value` double NOT NULL,
+  `semester` varchar(10) NOT NULL,
   PRIMARY KEY (`id_grade`),
   KEY `id_disc` (`id_disc`),
   KEY `registration_user` (`registration_user`),
@@ -66,7 +81,7 @@ CREATE TABLE `CD_GRADE` (
 ) ;
 
 CREATE TABLE `CD_MATERIAL` (
-  `id_disc` int(11) DEFAULT NULL,
+  `id_disc` int(11) NOT NULL,
   `material` blob,
   KEY `id_disc` (`id_disc`),
   CONSTRAINT `CD_MATERIAL_ibfk_1` FOREIGN KEY (`id_disc`) REFERENCES `CD_DISC` (`id_disc`)
@@ -74,8 +89,8 @@ CREATE TABLE `CD_MATERIAL` (
 
 CREATE TABLE `CD_QUESTION` (
   `id_question` int(11) NOT NULL AUTO_INCREMENT,
-  `question` varchar(255) DEFAULT NULL,
-  `options` varchar(600) DEFAULT NULL,
-  `answer` varchar(5) DEFAULT NULL,
+  `question` varchar(255) NOT NULL,
+  `options` varchar(600) NOT NULL,
+  `answer` varchar(5) NOT NULL,
   PRIMARY KEY (`id_question`)
 )
--- project-files/insert_lines.sql
+++ project-files/insert_lines.sql
@@ -1,6 +1,28 @@
 use school_life;
-
 INSERT INTO school_life.CD_ROLE (role_name) VALUES ('ROLE_ADMIN');
 INSERT INTO school_life.CD_ROLE (role_name) VALUES ('ROLE_ALUNO');
 INSERT INTO school_life.CD_ROLE (role_name) VALUES ('ROLE_PROF');
 INSERT INTO school_life.CD_ROLE (role_name) VALUES ('ROLE_RESPON');
+
+INSERT INTO `school_life`.`CD_USER`(`registration_user`,`email`,`password`,`name`,`birthDate`,`docs`,`id_role`)
+VALUES(123456,'aluno@aluno.com','123','aluno',date(sysdate()),'',2);
+
+INSERT INTO `school_life`.`CD_USER`(`registration_user`,`email`,`password`,`name`,`birthDate`,`docs`,`id_role`)
+VALUES(123457,'prof@prof.com','123','professor',date(sysdate()),'',3);
+
+INSERT INTO `school_life`.`CD_USER`(`registration_user`,`email`,`password`,`name`,`birthDate`,`docs`,`id_role`)
+VALUES(123458,'admin@admin.com','123','administrador',date(sysdate()),'',1);
+
+INSERT INTO `school_life`.`CD_USER`(`registration_user`,`email`,`password`,`name`,`birthDate`,`docs`,`id_role`)
+VALUES(123459,'resp@resp.com','123','responsavel',date(sysdate()),'',4);
+
+INSERT INTO `school_life`.`CD_DISC` (`name`,`description`) VALUES ('matematica','5º Ano');
+INSERT INTO `school_life`.`CD_DISC` (`name`,`description`) VALUES ('portugues','5º Ano');
+INSERT INTO `school_life`.`CD_DISC` (`name`,`description`) VALUES ('ciencias','5º Ano');
+INSERT INTO `school_life`.`CD_DISC` (`name`,`description`) VALUES ('historia','5º Ano');
+INSERT INTO `school_life`.`CD_DISC` (`name`,`description`) VALUES ('geografia','5º Ano');
+
+INSERT INTO school_life.RL_STUDENT_DISC (registration_user,id_disc) values (123456,2);
+INSERT INTO school_life.RL_STUDENT_DISC (registration_user,id_disc) values (123456,3);
+
+INSERT INTO school_life.RL_STUDENT_RESP (registration_user, registration_user_resp) values (123456, 123459);
